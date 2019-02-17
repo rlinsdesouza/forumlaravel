fefa2a9c4480c480cd2ddb1f0f3ae6f08d1e6959
rlinsdesouza <rlinsdesouza@gmail.com>
2019-02-17 17:18:47 -0300

    css public

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,8 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    {{-- <script src="js/app.js" defer></script>
-    <script src="js/offcanvas.js" defer></script> --}}
+    <script src="js/app.js" defer></script>
+    <script src="js/offcanvas.js" defer></script>
     
 
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -26,9 +26,9 @@
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css"/>  
 
     <!-- Styles -->
-    {{-- <link href="css/app.css" rel="stylesheet">
+    <link href="css/app.css" rel="stylesheet">
     <link href="css/offcanvas.css" rel="stylesheet">
-    <link href="css/custom.css" rel="stylesheet"> --}}
+    <link href="css/custom.css" rel="stylesheet">
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/offcanvas.css') }}" rel="stylesheet">
